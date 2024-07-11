7f165b8763b4bb377b92dfc497b99e463939283a
Sari <sarypc00@gmail.com>
2024-07-11 21:07:22 +0300

    added packages

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
+const bodyParser = require("body-parser");
 const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+require("dotenv/config");
 
 const app = express();
-
-const hostname = "0.0.0.0";
-const port = 3000;
+const env = process.env;
 app.listen(port, hostname, (req, res) => {
-  console.log(`Server running at http://${hostname}:${port}`);
+  console.log(
+    `Server running at http://${process.env.HOSTNAME}:${process.env.PORT}`
+  );
 });
